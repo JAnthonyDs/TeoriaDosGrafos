4f06e3da459f066cdb468816f457374bc86f5b22
José Anthony Dantas Santana <42504892+JAnthonyDs@users.noreply.github.com>
2023-05-23 22:13:41 -0300

    add flags FloydWarshall.cpp

--- FloydWarshall.cpp
+++ FloydWarshall.cpp
@@ -5,6 +5,97 @@ using namespace std;
 #define INFINITO 1000000
 
 int main(){
+    string input_file = "";
+    string output_file = "";
+    bool show_solution = false;
+    int source = 1;
+    // Parsing command line arguments
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-h") == 0)
+        {
+            cout << "Help:" << endl;
+            cout << "-h: mostra o help" << endl;
+            cout << "-o <arquivo>: redireciona a saida para o 'arquivo'" << endl;
+            cout << "-f <arquivo>: indica o 'arquivo' que contém o grafo de entrada" << endl;
+            cout << "-s: mostra a solução (em ordem crescente)" << endl;
+            cout << "-i: vértice inicial (para o algoritmo de Prim)" << endl;
+                return 0;
+        }
+        else if (strcmp(argv[i], "-o") == 0 && i < argc - 1)
+        {
+            output_file = argv[++i];
+        }
+        else if (strcmp(argv[i], "-f") == 0 && i < argc - 1)
+        {
+            input_file = argv[++i];
+        }
+        else if (strcmp(argv[i], "-s") == 0)
+        {
+            show_solution = true;
+        }
+        else if (strcmp(argv[i], "-i") == 0 && i < argc - 1)
+        {
+            source = atoi(argv[++i]);
+        }
+    }
+
+    // If no input file is specified, exit the program.
+    if (input_file == "")
+    {
+        cerr << "No input file specified. Use the -f parameter." << endl;
+        return 1;
+    }
+
+    ifstream fin(input_file);
+    if (!fin)
+    {
+        cerr << "Could not open input file: " << input_file << endl;
+        return 1;
+    }
+
+    int n, m;
+    fin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        fin >> u >> v >> w;
+        adj[u].push_back(make_pair(v, w));
+        adj[v].push_back(make_pair(u, w));
+    }
+
+    fin.close();
+
+    if (!(output_file == ""))
+    {
+        ofstream fout(output_file);
+        if (!fout)
+        {
+            cerr << "Could not open output file: " << output_file << endl;
+            return 1;
+        }
+
+        std::sort(mst.begin(), mst.end());
+
+        for (int i = 0; i < min((int)mst.size() - 1, m); i++)
+        {
+            fout << "(" << mst[i].first << "," << mst[i].second << ") ";
+        }
+        cout << endl;
+
+        fout.close();
+    }
+
+    if (show_solution)
+    {
+        std::sort(mst.begin(), mst.end());
+
+        for (int i = 0; i < min((int)mst.size() - 1, m); i++)
+        {
+            cout << "(" << mst[i].first << "," << mst[i].second << ") ";
+        }
+        cout << endl;
+    }
 
     int d0[5][5] = {
         {0,3,8,INFINITO,-4},
@@ -30,7 +121,7 @@ int main(){
         {-1,-1,0,5,-1}
     }; //Arestas que passa
 
-    //Floyd-Warshall
+    //Algoritmo de Floyd-Warshall
 
     for(int k = 0; k < 5; k++){
         for(int i = 0; i < 5; i++){
